e59cd0869abab266c3e7bc22539b7f40ea085906
wambui7 <shiko.sheila@gmail.com>
2018-08-14 17:39:28 -0500

    no change

--- names/Program.cs
+++ names/Program.cs
@@ -7,6 +7,26 @@ namespace names
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter your first name!");
+            string firstname = Console.ReadLine();
+            Console.WriteLine("Enter your last name!");
+            string lastname = Console.ReadLine();
+
+            string fullnames = Combine(firstname, lastname);
+
+            Console.WriteLine(fullnames);
+
         }
+        private static string combine(string firstname, string lastname);
+
+
     }
-}
+        
+   {
+         string fullnames = string firstname + string lastname;
+          return fullnames;
+    }
+
+        
+    
+
